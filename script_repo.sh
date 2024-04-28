56bf83f5fc13bc93d70d4de2a398ba1be190b4f1
jerinyasmin <jerin_cse03@yahoo.com>
2024-04-28 13:00:47 -0400

    Add files via upload

---
+++ script_repo.sh
@@ -0,0 +1,1 @@
+src search "context:global select:repo visibility:public count:all lang:Python content:'from transformers' AND from_pretrained\(" > result_repo_fromimport_transformers_from_pretrained.csv
